cd6ace3bca08b44cf06dc5228f2860caa95a3988
SIMON Y <simonyang81@gmail.com>
2021-01-08 10:57:17 +0800

    menu 3

--- Practice-SwiftUI/NVGView.swift
+++ Practice-SwiftUI/NVGView.swift
@@ -8,17 +8,18 @@
 import SwiftUI
 
 struct NVGView: View {
+
+    @State var password = ""
+
     var body: some View {
-        NavigationView {
-            VStack {
-                NavigationLink(destination: ButtonView(), label: {
-                    Text("push")
-                }).navigationBarTitle(Text("mark"), displayMode: .inline)
-                Spacer()
-            }.border(Color.red)
-                    
+        VStack {
+            Text("VStack Item 1")
+            Text("VStack Item 2")
+            Divider().background(Color.black)
             Spacer()
-        }
+            SecureField("Enter a password", text: $password).border(Color.black).padding()
+            Text("VStack Item 3")
+        }.background(Color.blue)
     }
 }
 
